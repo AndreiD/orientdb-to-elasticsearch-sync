f1d2f2dbef176b99dce4391850662b8a945457fc
Dan <danandreist@gmail.com>
2015-02-11 17:27:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config(object):
     ORIENTDB_SERVER_URL = 'http://localhost'
     ORIENTDB_SERVER_PORT = '5551'
     ORIENTDB_USER = 'root'
-    ORIENTDB_PASSWORD = 'parola123'
-    ORIENTDB_DB_NAME = 'FaceBook_Scrapper'
+    ORIENTDB_PASSWORD = 'your-password-here'
+    ORIENTDB_DB_NAME = 'GreatfulDeadConcerts'
 
     #------- ELASTICSERCH Config ----------
     ES_SERVER_URL = 'http://localhost'
